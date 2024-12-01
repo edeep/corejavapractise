637a3ac278f2b73dd2a1100a2009a8d738abeb24
edeep <pradeep.dhavakumar@gmail.com>
2024-12-01 20:17:37 +0530

    Sample File for Jenkins Trigger

---
+++ src/pradeeptest/HelloJenkins.java
@@ -0,0 +1,11 @@
+package pradeeptest;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Jenkins");
+
+	}
+
+}
