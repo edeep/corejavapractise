a6407bf182c39f084495a727c6836a843ccc4c1f
edeep <pradeep.dhavakumar@gmail.com>
2024-12-01 20:23:42 +0530

    Test Jenkins Trigger 1

--- src/pradeeptest/HelloJenkins.java
+++ src/pradeeptest/HelloJenkins.java
@@ -5,7 +5,7 @@ public class HelloJenkins {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Jenkins");
-
+		System.out.println("Added for Jenkins to trigger build");
 	}
 
 }
